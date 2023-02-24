eb7db4bb26401fa539b41fbf1b98a010f8b19ac9
Danielle Lei <Danielle.lei2014@gmail.com>
2023-02-24 12:38:20 -0800

    edited spot migration

--- backend/db/migrations/20230221190601-create-spot.js
+++ backend/db/migrations/20230221190601-create-spot.js
@@ -4,10 +4,7 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 /** @type {import('sequelize-cli').Migration} */
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable('Spots', {
--- backend/db/seeders/20230216211212-demo-user.js
+++ backend/db/seeders/20230216211212-demo-user.js
@@ -53,9 +53,7 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    return queryInterface.bulkDelete(options, null, {});
     //await queryInterface.bulkDelete('Users', null)
   }
 };
