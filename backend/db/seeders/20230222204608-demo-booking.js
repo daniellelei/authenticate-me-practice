2766edc5effd46c2c249bb5730ef9a28a7202f4a
Danielle Lei <Danielle.lei2014@gmail.com>
2023-02-26 02:29:37 -0800

    fixed seeder files

--- backend/db/seeders/20230222204608-demo-booking.js
+++ backend/db/seeders/20230222204608-demo-booking.js
@@ -11,8 +11,8 @@ module.exports = {
    return queryInterface.bulkInsert(options, [
     {spotId:1, userId:4, startDate:"2024-03-17" , endDate:"2024-03-19"},
     {spotId:2, userId:3, startDate:"2023-04-17" , endDate:"2023-04-18" },
-    {spotId:3, userId:2, startDate:"2023-03-15" , endDate:"2022-04-16" },
-    {spotId:4, userId:1, startDate:"2023-06-17" , endDate:"2022-06-28" },
+    {spotId:3, userId:2, startDate:"2023-03-15" , endDate:"2023-04-16" },
+    {spotId:4, userId:1, startDate:"2023-06-17" , endDate:"2023-06-28" },
   
    ], {})
   },
