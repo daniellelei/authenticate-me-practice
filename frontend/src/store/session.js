f37aea743a8065a3ed67c2f8f0afc3d3f171bcea
Danielle Lei <Danielle.lei2014@gmail.com>
2023-03-11 12:27:11 -0800

    authenticate me

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -18,6 +18,7 @@ const removeUser = () => {
   };
 };
 
+//thunk
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
@@ -36,6 +37,32 @@ export const restoreUser = () => async dispatch => {
   const response = await csrfFetch('/api/session');
   const data = await response.json();
   dispatch(setUser(data.user));
+  return response;
+};
+
+export const signup = (user) => async dispatch => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+//logout
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
   return response;
 };
 
