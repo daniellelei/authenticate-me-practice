4fa66151b4732b85106b18e3a0ed6269d7550761
Danielle Lei <Danielle.lei2014@gmail.com>
2023-05-20 16:48:01 -0700

    google map setup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,14 +4,15 @@ import sessionReducer from './session';
 import spotsReducer from "./spots";
 import reviewsReducer from "./reviews";
 import bookingsReducer from "./bookings";
+import mapsReducer from "./maps";
 
 const rootReducer = combineReducers({
   // add reducer functions here
   session: sessionReducer,
   spots: spotsReducer,
   reviews: reviewsReducer,
-  bookings:bookingsReducer
-
+  bookings:bookingsReducer,
+  maps:mapsReducer
 });
 
 let enhancer;
