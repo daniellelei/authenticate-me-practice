61e77fa1688f3a2e5bbdb7d10d45f13e3c600c8b
Danielle Lei <Danielle.lei2014@gmail.com>
2023-05-21 14:59:34 -0700

    setup geocode

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import { useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
-
+import Geocode from 'react-geocode'
 const containerStyle = {
   width: '400px',
   height: '400px',
@@ -15,13 +15,37 @@ const center = {
 };
 
 const Maps = ({ apiKey }) => {
+
+
+  const [map, setMap] = useState(null)
+  const spot = useSelector(state=>state.spots.singleSpot)
+  const address = spot.address.concat(", ", spot.city).concat(", ", spot.state)
+
+  Geocode.setApiKey(apiKey);
+  Geocode.setLanguage("en");
+  Geocode.setLocationType('ROOFTOP')
+  Geocode.enableDebug();
+
+  const [currentPosition, setCurrentPosition] = useState();
+
+  const makeMap = (e) => {
+    e.preventDefault()
+    Geocode.fromAddress(address).then(
+      (response) => {
+        const {lat, lng} = response.results[0].geometry.location 
+        setCurrentPosition({lat, lng})
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: apiKey,
   });
 
-  const [map, setMap] = useState(null)
-  const spot = useSelector(state=>state.spots.singleSpot)
+  
   
   const onUnmount = useCallback(function callback(map) {
     setMap(null)
@@ -29,20 +53,25 @@ const Maps = ({ apiKey }) => {
 
   return (
     <>
-      {isLoaded && (
-        <GoogleMap
+      {isLoaded && <GoogleMap
           mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
+          zoom={8}
+          center={currentPosition}
           onUnmount={onUnmount}
-        >
-          <Marker 
-              position={center}
-              title="spot"
-              icon={spot.SpotImages[0]}
+          >
+        <Marker 
+              position={currentPosition}
+              title="Destination"
+              icon={{
+                path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+                fillColor: '#db8ba3',
+                fillOpacity: 1,
+                scale: .2,
+                strokeColor: 'gold',
+                strokeWeight: 2
+              }}
               streetView={false} />
-        </GoogleMap>
-      )}
+        </GoogleMap>}
     </>
   );
 };
